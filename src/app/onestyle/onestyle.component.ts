9aa0536cecaca0680c53881937f696a410a44cda
blumi <b4105236@gmail.com>
2021-11-28 15:29:09 +0200

    onestyle

---
+++ src/app/onestyle/onestyle.component.ts
@@ -0,0 +1,9 @@
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-onestyle',
+  templateUrl: './onestyle.component.html',
+  styleUrls: [ './onestyle.component.css' ],
+})
+export class onestyleComponent  {
+}
